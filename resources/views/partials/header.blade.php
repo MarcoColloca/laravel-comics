5cea48c74e20d6b9cdaf663368ed2ac3e00dee26
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-21 15:33:29 +0200

    Add → added header.blade partial file

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="container">
+        <nav class="py-5">
+            <div class="row">
+                <div class="col">
+                    <div>
+                        Site Title
+                    </div>
+                </div>
+
+                <div class="col">
+                    <ul class="nav-bar">
+                        <li class="mx-3">
+                            <a href="">Home</a>
+                        </li>
+                        <li class="mx-3">
+                            <a href="">Pagina 2</a>
+                        </li>
+                        <li class="mx-3">
+                            <a href="">Pagina 3</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <div class="text-end">
+                        Logo
+                    </div>
+                </div>                
+            </div>
+        </nav>
+    </div>
+</header>
