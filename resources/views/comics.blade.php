cb09415362608aca23ea24734161da12cb0bf5aa
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-21 15:39:24 +0200

    Feat → added comics.blade page as extension of app.blade

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Comics')
+
+@section('main-content')
+    <section>
+        <div class="container">
+            <h1 class="title text-danger">Comics!</h1>
+        </div>
+    </section>    
+@endsection
