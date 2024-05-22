45992fdb5073106f604d5ceb8e744d4e3b4e0182
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-22 17:47:13 +0200

    Add → added mt to welcome.blade section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     </section>
 
 
-    <section class="py-1">
+    <section class="mb-5 py-1">
         <div class="bg-light container py-4">
             <p class="title text-center text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam expedita quo hic at ratione fuga asperiores nemo quae aspernatur rerum, aut repellat. Molestiae dolores ad est, maiores at provident velit dolor, illum amet harum quo quibusdam doloribus quisquam ut quasi? Non repudiandae cum doloribus aperiam minus, porro dolores suscipit saepe odit voluptas ad culpa alias dolorem repellat accusantium excepturi, beatae ipsum fuga nobis quidem quos repellendus exercitationem iure? Id asperiores nam commodi facilis nihil magni fugiat atque esse delectus officia, minus sapiente accusantium, quos quibusdam optio, ut incidunt eveniet expedita totam aspernatur nobis ratione dolor. Sequi molestias tempore doloribus?</p>
         </div>
