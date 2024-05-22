18bb0b598ccafd7cd27e32a05a893708af56e18f
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-22 17:45:12 +0200

    Add → added footer dc style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,125 @@
-<footer class="py-3 bg-info">
-    <h1 class="text-center">
-        Un foteeeeeeeeeeeeeeeeeeer
-    </h1>
+<footer>
+    <section class="footer-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 py-3">
+                    <div class="footer-lists-box">
+                        <ul>
+                            <li>
+                                <h2>DC COMICS</h2>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                        </ul>
+            
+                        <ul>
+                        <li>
+                                <h2>DC</h2>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+            
+                        </ul>
+            
+                        <ul>
+                        <li>
+                                <h2>SITES</h2>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                        </ul>
+            
+                        <ul>
+                            <li>
+                                <h2>SHOP</h2>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                            <li>
+                                <a href="">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="logo-container"> <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="p-5">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div  class="footer-sing-up">
+                    <button>SING UP NOW!</button>
+                </div>
+
+                <div class="d-flex gap-2">
+                    <h4>FOLLOW US</h4> 
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt=""></a>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt=""></a> 
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt=""></a> 
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
