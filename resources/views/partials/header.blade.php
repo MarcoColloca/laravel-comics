dd7569d79d0954307dbb0bde20c60b98c3861c5a
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-22 16:32:46 +0200

    Add → added DC header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,35 +1,47 @@
-<header class="bg-primary text-light">
-    <div class="container">
-        <nav class="py-5">
-            <div class="row">
-                <div class="col">
-                    <div>
-                        <h1 class="text-start">
-                            Laravel!
-                        </h1>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <ul class="nav-bar">
-                        <li class="mx-3">
-                            <a class="text-white" href="{{route('home')}}">Home</a>
-                        </li>
-                        <li class="mx-3">
-                            <a class="text-white" href="{{route('comics')}}">Comics</a>
-                        </li>
-                        <li class="mx-3">
-                            <a class="text-white" href="{{route('about')}}">About Us</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col">
-                    <div class="text-end nav-logo">
-                        <img src="/img/bilancia.png" alt="">
-                    </div>
-                </div>                
+<header>
+    <div class="main-header__others">
+        <div class="container">
+            <div class="w-100 d-flex justify-content-end gap-5 p-1">
+                <span>
+                    DC POWER VISA
+                </span>
+                <span>
+                    <a href="">ADDITIONAL DC SITES ►</a>
+                </span>
             </div>
-        </nav>
+        </div>
     </div>
+    <section  class="main-header__nav">        
+        <div class="container">
+            <nav class="py-3">
+                <div class="row">
+                    <div class="col">
+                        <div class="text-start nav-logo">
+                            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
+                        </div>
+                    </div>                
+                    
+                    <div class="col">
+                        <ul class="nav-bar">
+                            <li class="mx-4">
+                                <a href="{{route('home')}}">HOME</a>
+                            </li>
+                            <li class="mx-4">
+                                <a href="{{route('comics')}}">COMICS</a>
+                            </li>
+                            <li class="mx-4">
+                                <a href="{{route('about')}}">ABOUT US</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div class="col">
+                        <div class="text-end nav-search">
+                            <input type="text" placeholder="Search">
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
 </header>
