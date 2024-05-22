d5924da91d4bd142247c24839c1e8a52a48393ca
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-22 15:52:44 +0200

    Add → added comics card generation with foreach

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,9 +3,30 @@
 @section('title', 'Comics')
 
 @section('main-content')
-    <section class="mt-5">
+    <section class="my-5 p-3">
         <div class="container">
-            <h1 class="title text-center text-danger">Comics!</h1>
+            <h1 class="title text-center text-danger pb-5">Comics!</h1>
+        </div>
+
+        <div class="container">
+            <div class="row g-5">
+                @foreach ($comics as $comic)
+                    <div class="col-4">
+                        <div class="card my-card">
+                            <img src="{{url($comic['thumb'])}}" class="card-img-top p-5 pb-2" alt="...">
+
+                            <div class="card-body px-5">
+                                <h5 class="card-title text-center pb-3">{{$comic['title']}}</h5>                            
+                            </div>
+
+                            <div class="card-footer px-5">                                
+                                <p class="card-text text-center">{{$comic['series']}}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                @endforeach                
+            </div>
         </div>
     </section>    
 @endsection
